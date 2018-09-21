590a490c886caad3cace15bd7c5d5555049aa84c
cho45 <cho45@lowreal.net>
2018-09-21 23:19:12 +0900

    add drawing image

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 
 const {Bus, Device} = require('i2c-bus-promised');
+const Canvas = require('canvas');
+const BDFFont = require('bdf-canvas').BDFFont;
 
 function wait(n) { return new Promise( (r) => setTimeout(r, n)) }
 
@@ -176,6 +178,7 @@ class OLED {
 const fs_ = require('fs');
 const fs = fs_.promises;
 fs.watch = fs_.watch;
+fs.createWriteStream = fs_.createWriteStream;
 
 async function watchInterrupt(pin, func) {
 	await fs.writeFile(`/sys/class/gpio/gpio${pin}/direction`, 'in');
@@ -202,6 +205,50 @@ async function watchInterrupt(pin, func) {
 	};
 }
 
+function convertToBinary(ctx, x, y, w, h) {
+	const imagedata = ctx.getImageData(x, y, w, h);
+	const data = imagedata.data;
+	// Conver to grayscale (use RED position for retaining pixel)
+	for (let x = 0, xm = imagedata.width; x < xm; x++) {
+		for (let y = 0, ym = imagedata.height; y < ym; y++) {
+			const i = (y * xm + x) * 4;
+			const r = data[i+0];
+			const g = data[i+1];
+			const b = data[i+2];
+			const gray = 0.3 * r + 0.59 * g + 0.11 * b;
+			data[i+0] = gray;
+		}
+	}
+	// Floyd-Steinberg dithering
+	let error = 0;
+	const f7 = 7/16, f3 = 3/16, f5 = 5/16, f1 = 1/16;
+	for (let y = 0, ym = imagedata.height; y < ym; y++) {
+		for (let x = 0, xm = imagedata.width; x < xm; x++) {
+			const i = (y * xm + x) * 4;
+			const gray = data[i+0]; // get gray data from RED position (see above)
+			const val  = gray < 127 ? 0 : 255; // quantize to binary
+			data[i+0] = val;
+			data[i+1] = val;
+			data[i+2] = val;
+			error = gray - val;
+			data[((y+0) * xm + (x+1)) * 4] += f7 * error;
+			data[((y+1) * xm + (x-1)) * 4] += f3 * error;
+			data[((y+1) * xm + (x+0)) * 4] += f5 * error;
+			data[((y+1) * xm + (x+1)) * 4] += f1 * error;
+		}
+	}
+	return imagedata;
+}
+
+async function loadImage(path) {
+	return await new Promise( async (resolve, reject) => {
+		const img = new Canvas.Image();
+		img.onload = () => { resolve(img) };
+		img.onerror = reject;
+		img.src = await fs.readFile(path, null);
+	});
+}
+
 
 async function main() {
 	const bus = new Bus(0);
@@ -211,18 +258,17 @@ async function main() {
 	await oled.initialize();
 	await oled.clear();
 
-	const Canvas = require('canvas');
-	const BDFFont = require('bdf-canvas').BDFFont;
-	console.log(BDFFont);
-
 	const font = new BDFFont(await fs.readFile("./mplus_f10r.bdf", "utf-8"));
+
+	// const WHITE = "rgb(130, 244, 248)";
+	const WHITE = "#ffffff";
 
 	const canvas = new Canvas(128, 64);
 	const ctx = canvas.getContext("2d");
 	ctx.fillStyle = "#000";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-	ctx.fillStyle = "rgb(130, 244, 248)";
+	ctx.fillStyle = WHITE;
 	const lineHeight = 12;
 	font.drawText(ctx, "123456789012345678901", 1, lineHeight*1-2);
 	font.drawText(ctx, "123456789012345678901", 1, lineHeight*2-2);
@@ -231,6 +277,22 @@ async function main() {
 
 	font.drawText(ctx, "OK         <        >", 1, 60);
 
+	ctx.fillStyle = "#000";
+	ctx.fillRect(0, 0, 128, 64);
+
+	ctx.fillStyle = WHITE;
+	font.drawText(ctx, "preventDef", 65, lineHeight*1-2);
+	font.drawText(ctx, "2345678901", 65, lineHeight*2-2);
+	font.drawText(ctx, "2345678901", 65, lineHeight*3-2);
+	font.drawText(ctx, "2345678901", 65, lineHeight*4-2);
+	font.drawText(ctx, "2345678901", 65, lineHeight*5-2);
+
+	const img = await loadImage("./foo.jpg");
+	ctx.drawImage(img, 0, 0, 64, 64);
+
+	const id = convertToBinary(ctx, 0, 0, 64, 64);
+	ctx.putImageData(id, 0, 0);
+
 	oled.drawImage(ctx.getImageData(0, 0, 128, 64));
 
 	const interruptHandler = (val, pin) => {
@@ -238,15 +300,26 @@ async function main() {
 
 		ctx.fillStyle = "#000";
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
-		ctx.fillStyle = "rgb(130, 244, 248)";
+		ctx.fillStyle = WHITE;
 		const lineHeight = 12;
 		font.drawText(ctx, `${name} clicked`, 1, lineHeight*1-2);
 		oled.drawImage(ctx.getImageData(0, 0, 128, 64));
 	};
 
-	watchInterrupt(0, interruptHandler); // F1
-	watchInterrupt(2, interruptHandler); // F2
-	watchInterrupt(3, interruptHandler); // F3
+	const out = fs.createWriteStream('text.png')
+	const stream = canvas.pngStream();
+
+	stream.on('data', function(chunk){
+		out.write(chunk);
+	});
+
+	stream.on('end', function(){
+		console.log('saved png');
+	});
+
+//	watchInterrupt(0, interruptHandler); // F1
+//	watchInterrupt(2, interruptHandler); // F2
+//	watchInterrupt(3, interruptHandler); // F3
 }
 
 main();
